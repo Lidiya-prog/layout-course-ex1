439e2a6c48016cf9a83d5d504447d0048ea678ab
Lidiya Morshchinina <lmorshchinka@gmail.com>
2023-07-16 08:58:28 +0300

    add modal and gallery

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems);
+
+  var modals = document.querySelectorAll('.modal');
+  var modalInstances = M.Modal.init(modals);
+
+  var boxes = document.querySelectorAll('.materialboxed');
+  var boxeaInstances = M.Materialbox.init(boxes);
 });
